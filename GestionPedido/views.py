3d5d98404da08450002d49147ea86805c13bd0ee
Victor Marquez <marquez.pevic21@cadiz.salesianos.edu>
2023-02-21 18:41:04 +0100

    formulario de busqueda

--- GestionPedido/views.py
+++ GestionPedido/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse 
+from GestionPedido import models
 # Create your views here.
+
+
+def busqueda_productos(request):
+    
+    return render(request, "busqueda_productos.hmtl")
+
+def buscar(request):
+    if request.GET["prd"]:
+
+       # mensaje="Articulo buscado: %r" %request.GET["prd"]
+       producto=request.GET["prd"]
+
+       articulos=models.Articulos.objects.filter(nombre__icontains=producto)
+
+       return render(request,"resultados_busqueda.html",{"articulos":articulos, "query":producto})
+
+    else:
+        mensaje="no has introducido nada"
+    return HttpResponse(mensaje)
